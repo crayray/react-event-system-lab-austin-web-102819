fd9b35521b0b73ccb9741ae17bd140c24d4f4c41
rachfuerst <rach.fuerst@gmail.com>
2020-01-09 07:12:27 -0600

    Completed but for some reason it's not working

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+class EyesOnMe extends React.Component {
+    focusEvent = () => console.log('Good!')
+        blurEvent = () => console.log('Hey! Eyes on me!')
+    render () {
+        return (
+            <div>
+            <button focusEvent={this.focusEvent} blurEvent={this.blurEvent}>Button</button>
+           </div>
+        );
+    }
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,13 @@
 // Code Keypad Component Here
+import React from 'react';
+
+class Keypad extends React.component {
+    logInput = () => console.log('Entering password...')
+    
+    render () {
+        return (
+            <input type="password" onKeyUp={this.logInput}  />
+        );
+    }
+}
+export default Keypad;
